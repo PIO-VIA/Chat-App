74de754e960ae389978a994d7640d61b59041e20
TAGATSING Samuel <samuelftagat@gmail.com>
2025-05-04 03:41:44 +0100

    Initialisation de la BD locale

--- src/main/java/org/personnal/client/database/DAO/UserDAO.java
+++ src/main/java/org/personnal/client/database/DAO/UserDAO.java
@@ -1,4 +1,23 @@
 package org.personnal.client.database.DAO;
 
+import org.personnal.client.database.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class UserDAO {
+    public static void adduser(String username, String email, String pass){
+        String sql="insert into users(username, email, password_hash) values(?, ?, ?)";
+        try (Connection conn=DatabaseConnection.connect();
+             PreparedStatement stmt= conn.prepareStatement(sql)){
+            stmt.setString(1,username);
+            stmt.setString(2,email);
+            stmt.setString(3,pass);
+            stmt.executeUpdate();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/org/personnal/client/database/DatabaseConnection.java
+++ src/main/java/org/personnal/client/database/DatabaseConnection.java
@@ -1,4 +1,10 @@
 package org.personnal.client.database;
+import java.sql.*;
 
 public class DatabaseConnection {
+    private static final String DB_URL = "jdbc:sqlite:chat.db";
+
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection(DB_URL);
+    }
 }
